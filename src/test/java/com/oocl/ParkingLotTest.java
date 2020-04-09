bc9904a284bedc1d14d86ca7113965e28f2cc61b
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-09 15:03:32 +0800

    extract setting part from testcase to @Before

--- src/test/java/com/oocl/ParkingLotTest.java
+++ src/test/java/com/oocl/ParkingLotTest.java
@@ -1,13 +1,20 @@
 package com.oocl;
 
 import junit.framework.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
+    ParkingLot parkingLot;
+    Car car;
+    @Before
+    public void setup(){
+        parkingLot = new ParkingLot();
+        car = new Car();
+    }
+
     @Test
     public void should_return_ticket_when_part_car(){
-        ParkingLot parkingLot = new ParkingLot();
-        Car car = new Car();
         ParkingTicket ticket = parkingLot.park(car);
         Assert.assertNotNull(ticket);
     }
