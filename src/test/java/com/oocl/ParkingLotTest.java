15afc44aa9691019046b6f7c1b20daa4e5844a9f
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-09 15:10:24 +0800

    passed the test - should_not_return_car_when_providing_invalid_ticket

--- src/test/java/com/oocl/ParkingLotTest.java
+++ src/test/java/com/oocl/ParkingLotTest.java
@@ -4,6 +4,8 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.core.IsNot.not;
+
 public class ParkingLotTest {
     ParkingLot parkingLot;
     Car car;
@@ -23,5 +25,12 @@ public class ParkingLotTest {
     public void should_return_car_when_provide_corresponding_ticket(){
         ParkingTicket ticket = parkingLot.park(car);
         Assert.assertEquals(car, parkingLot.fetch(ticket));
+    }
+
+    @Test
+    public void should_not_return_car_when_providing_invalid_ticket(){
+        ParkingTicket ticket = parkingLot.park(car);
+        ParkingTicket fakeTicket = new ParkingTicket();
+        Assert.assertFalse(car.equals(parkingLot.fetch(fakeTicket)));
     }
 }
