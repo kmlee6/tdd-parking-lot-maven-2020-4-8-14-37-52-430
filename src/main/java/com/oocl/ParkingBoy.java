1f3ed3ef953d60ff05bf10c185b3c145ef58cd41
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-10 18:47:08 +0800

    [ParkingBoy] Roll back to single parking lot version

--- src/main/java/com/oocl/ParkingBoy.java
+++ src/main/java/com/oocl/ParkingBoy.java
@@ -5,18 +5,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ParkingBoy {
-    private List<ParkingLot> parkingLots = new ArrayList<ParkingLot>();
-
-    public ParkingBoy(ParkingLot... parkingLots){
-        this.parkingLots.addAll(Arrays.asList(parkingLots));
+//    private List<ParkingLot> parkingLots = new ArrayList<ParkingLot>();
+    private ParkingLot parkingLot;
+    public ParkingBoy(ParkingLot parkingLots){
+//        this.parkingLots.addAll(Arrays.asList(parkingLots));
+        this.parkingLot = parkingLots;
     }
 
     public ParkingTicket park(Car car){
-        ParkingLot nextAvailableParkingLot = this.parkingLots.stream().filter(parkingLot -> !parkingLot.isFull()).findFirst().get();
-        return nextAvailableParkingLot.park(car);
+//        ParkingLot nextAvailableParkingLot = this.parkingLots.stream().filter(parkingLot -> !parkingLot.isFull()).findFirst().get();
+//        return nextAvailableParkingLot.park(car);
+        return parkingLot.park(car);
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
-        return parkingLots.get(0).fetch(parkingTicket);
+//        return parkingLots.get(0).fetch(parkingTicket);
+        return parkingLot.fetch(parkingTicket);
     }
 }
